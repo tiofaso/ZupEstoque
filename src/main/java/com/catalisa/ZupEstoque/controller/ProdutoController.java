493ee408438c060491326ffcf8ecff34283ef6cd
Fábio Luiz Sousa Vaz <fabio.vaz@zup.com.br>
2023-08-31 18:31:02 -0300

    :construction: Exception ainda não funciona para 404 e 500

--- src/main/java/com/catalisa/ZupEstoque/controller/ProdutoController.java
+++ src/main/java/com/catalisa/ZupEstoque/controller/ProdutoController.java
@@ -7,9 +7,16 @@ import com.catalisa.ZupEstoque.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import com.catalisa.ZupEstoque.exception.IdError;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -24,7 +31,7 @@ public class ProdutoController {
     ProdutoMapper produtoMapper;
 
     @PostMapping(path = "/cadastro") //Cadastra produto no estoque
-    public ResponseEntity<ProdutoDTO> cadastrarNovoProduto(@RequestBody Produto produto) {
+    public ResponseEntity<ProdutoDTO> cadastrarNovoProduto(@RequestBody @Valid Produto produto) {
         Produto novoProduto = produtoService.cadastraProdutoBase(produto);
         return new ResponseEntity<>(produtoMapper.toDto(novoProduto), HttpStatus.CREATED);
     }
@@ -41,10 +48,15 @@ public class ProdutoController {
     }
 
     @PutMapping(path = "/atualiza/{id}") //Atualiza determinado produto
-    public ResponseEntity<ProdutoDTO> atualizarProduto(@PathVariable Long id, @RequestBody Produto produto) {
-        Produto atualizaProduto = produtoService.atualizaProdutoBase(id,produto);
-        return new ResponseEntity<>(produtoMapper.toDto(atualizaProduto), HttpStatus.CREATED);
+    public ResponseEntity<ProdutoDTO> atualizarProduto(@PathVariable Long id, @RequestBody @Valid Produto produto) {
+        //try {
+            Produto atualizaProduto = produtoService.atualizaProdutoBase(id,produto);
+            return new ResponseEntity<>(produtoMapper.toDto(atualizaProduto), HttpStatus.CREATED);
 
+        //}catch (IdError ex) { ///Todo ainda não tá lançando erro no body
+         //   String mensagem = "Não foi enviado um ID para o sistema";
+            //throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Provide correct Actor Id", ex);
+        //}
     }
 
     @GetMapping(path = "/produto/{id}") //Exibe um produto específico
@@ -52,4 +64,17 @@ public class ProdutoController {
         return produtoService.buscarProdutoPorIdBase(id);
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return errors;
+    }
 }
