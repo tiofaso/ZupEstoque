031353976f342910506b1be7a20444dd5d89646e
Fábio Luiz Sousa Vaz <fabio.vaz@zup.com.br>
2023-08-31 18:31:35 -0300

    :construction: Atualizações gerais

--- src/main/java/com/catalisa/ZupEstoque/model/Produto.java
+++ src/main/java/com/catalisa/ZupEstoque/model/Produto.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
+
 
 @Entity
 @Table(name = "tb_produto")
@@ -19,15 +21,20 @@ public class Produto {
     private Long id;
 
     @Column(length = 50, nullable = false)
+    @NotBlank(message = "O nome do produto não pode estar em branco")
     private String nome;
 
     @Column(nullable = false)
+    @Positive(message = "A quantidade do produto não pode estar em branco")
     private int quantidade;
 
     @Column(nullable = false)
+    @Positive(message = "O preço do produto não pode estar em branco")
     private double preco;
 
     @Column(length = 255, nullable = false)
+    @NotBlank(message = "As informações do produto não podem estar em branco")
+    @Size(min = 5, max = 255, message = "Você pode digitar até 255 caracteres")
     private String informacoes;
 
     @Column(length = 3, nullable = false)
@@ -43,6 +50,8 @@ public class Produto {
     private String prateleira;
 
     @Column(length = 13, nullable = false)
+    @NotBlank(message = "O código de barras do produto não podem estar em branco")
+    @Size(min = 12,max = 13, message = "O código de barras tem pelo menos 12 números")
     private String codigoDeBarras;
 
     @Column(nullable = false)
